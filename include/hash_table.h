69538be7745a7d0d12125a21320276be77f0247f
Yuri Melo <yurifullstack@gmail.com>
2025-04-24 16:38:43 -0300

    Add HashTable implementation with linear search functionality

---
+++ include/hash_table.h
@@ -0,0 +1,22 @@
+#ifndef HASH_TABLE_H
+#define HASH_TABLE_H
+
+namespace hash_table
+{
+    typedef struct
+    {
+        char *key;
+        int value;
+    } item;
+
+    class HashTable
+    {
+    public:
+        HashTable();
+        ~HashTable();
+
+        item* linear_search(item *items, size_t size, const char *key);
+    };
+}
+
+#endif
---
+++ lib/hash_table/hash_table.cpp
@@ -0,0 +1,22 @@
+#include "../../include/hash_table.h"
+
+#include <iostream>
+
+namespace hash_table
+{
+    HashTable::HashTable(){}
+    HashTable::~HashTable(){}
+
+    item* HashTable::linear_search(item *items, size_t size, const char *key)
+    {
+        for (size_t index = 0; index < size; index++)
+        {
+            if (strcmp(items[index].key, key) == 0)
+            {
+                return &items[index];
+            }
+        }
+
+        return nullptr;
+    }
+}
